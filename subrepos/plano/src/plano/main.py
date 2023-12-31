81f883cb0087a90de1ef1a13c3798ab07e424954
Justin Ross <jross@apache.org>
2023-12-31 07:33:23 -0500

    git subrepo pull subrepos/plano
    subrepo:
      subdir:   "subrepos/plano"
      merged:   "c5800bf"
    upstream:
      origin:   "git@github.com:ssorj/plano.git"
      branch:   "main"
      commit:   "c5800bf"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

--- subrepos/plano/src/plano/main.py
+++ subrepos/plano/src/plano/main.py
@@ -20,6 +20,7 @@
 import base64 as _base64
 import binascii as _binascii
 import code as _code
+import datetime as _datetime
 import fnmatch as _fnmatch
 import getpass as _getpass
 import json as _json
@@ -45,7 +46,9 @@ _max = max
 ## Exceptions
 
 class PlanoException(Exception):
-    pass
+    def __init__(self, message=None):
+        super().__init__(message)
+        self.message = message
 
 class PlanoError(PlanoException):
     pass
@@ -310,8 +313,10 @@ def change_dir(dir, quiet=False):
     return prev_dir
 
 def list_dir(dir=None, include="*", exclude=()):
-    if dir in (None, ""):
+    if dir is None:
         dir = get_current_dir()
+    else:
+        dir = expand(dir)
 
     assert is_dir(dir), dir
 
@@ -343,6 +348,8 @@ class working_dir:
         if self.dir is None:
             self.dir = make_temp_dir()
             self.remove = True
+        else:
+            self.dir = expand(self.dir)
 
     def __enter__(self):
         if self.dir == ".":
@@ -407,7 +414,7 @@ def check_env(var, message=None):
 def check_module(module, message=None):
     if _pkgutil.find_loader(module) is None:
         if message is None:
-            message = "Module {} is not found".format(repr(module))
+            message = "Python module {} is not found".format(repr(module))
 
         raise PlanoError(message)
 
@@ -700,6 +707,9 @@ def parse_json(json):
 def emit_json(data):
     return _json.dumps(data, indent=4, separators=(",", ": "), sort_keys=True)
 
+def print_json(data, **kwargs):
+    print(emit_json(data), **kwargs)
+
 ## HTTP operations
 
 def _run_curl(method, url, content=None, content_file=None, content_type=None, output_file=None, insecure=False):
@@ -796,6 +806,7 @@ _DISABLED = _logging_levels.index("disabled")
 
 _logging_output = None
 _logging_threshold = _NOTICE
+_logging_contexts = list()
 
 def enable_logging(level="notice", output=None):
     assert level in _logging_levels
@@ -841,6 +852,16 @@ class logging_disabled(logging_enabled):
     def __init__(self):
         super().__init__(level="disabled")
 
+class logging_context:
+    def __init__(self, name):
+        self.name = name
+
+    def __enter__(self):
+        _logging_contexts.append(self.name)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        _logging_contexts.pop()
+
 def fail(message, *args):
     error(message, *args)
 
@@ -872,28 +893,39 @@ def log(level, message, *args):
         _print_message(level, message, args)
 
 def _print_message(level, message, args):
+    line = list()
     out = nvl(_logging_output, _sys.stderr)
-    exception = None
+
+    line.append(get_program_name())
+
+    level_color = ("cyan", "cyan", "cyan", "yellow", "red", None)[level]
+    level_bright = (False, False, False, False, True, False)[level]
+    level = cformat("{:>6}".format(_logging_levels[level]), color=level_color, bright=level_bright, file=out)
+
+    line.append(level)
+
+    for name in _logging_contexts:
+        line.append(name)
 
     if isinstance(message, BaseException):
         exception = message
-        message = "{}: {}".format(type(message).__name__, str(message))
+
+        line.append(type(exception).__name__)
+        line.append(str(exception))
+
+        print(": ".join(line), file=out)
+
+        if hasattr(exception, "__traceback__"):
+            _traceback.print_exception(type(exception), exception, exception.__traceback__, file=out)
     else:
         message = str(message)
 
-    if args:
-        message = message.format(*args)
+        if args:
+            message = message.format(*args)
 
-    program = "{}:".format(get_program_name())
+        line.append(capitalize(message))
 
-    level_color = ("cyan", "cyan", "blue", "yellow", "red", None)[level]
-    level_bright = (False, False, False, False, True, False)[level]
-    level = cformat("{:>6}:".format(_logging_levels[level]), color=level_color, bright=level_bright, file=out)
-
-    print(program, level, capitalize(message), file=out)
-
-    if exception is not None and hasattr(exception, "__traceback__"):
-        _traceback.print_exception(type(exception), exception, exception.__traceback__, file=out)
+        print(": ".join(line), file=out)
 
     out.flush()
 
@@ -1105,7 +1137,7 @@ def _format_command(command, represent=True):
     else:
         args = command
 
-    args = [expand(x) for x in args]
+    args = [expand(str(x)) for x in args]
     command = " ".join(args)
 
     if represent:
@@ -1159,14 +1191,14 @@ def start(command, stdin=None, stdout=None, stderr=None, output=None, shell=Fals
         if is_string(command):
             args = command
         else:
-            args = " ".join(command)
+            args = " ".join(map(str, command))
     else:
         if is_string(command):
             args = _shlex.split(command)
         else:
             args = command
 
-        args = [expand(x) for x in args]
+        args = [expand(str(x)) for x in args]
 
     try:
         proc = PlanoProcess(args, stdin=stdin, stdout=stdout, stderr=stderr, shell=shell, close_fds=True, stash_file=stash_file)
@@ -1310,7 +1342,7 @@ class PlanoProcess(_subprocess.Popen):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        kill(self)
+        stop(self)
 
     def __repr__(self):
         return "process {} (command {})".format(self.pid, _format_command(self.args))
@@ -1411,17 +1443,17 @@ def get_user_temp_dir():
     except KeyError:
         return join(get_system_temp_dir(), get_user())
 
-def make_temp_file(suffix="", dir=None):
+def make_temp_file(prefix="plano-", suffix="", dir=None):
     if dir is None:
         dir = get_system_temp_dir()
 
-    return _tempfile.mkstemp(prefix="plano-", suffix=suffix, dir=dir)[1]
+    return _tempfile.mkstemp(prefix=prefix, suffix=suffix, dir=dir)[1]
 
-def make_temp_dir(suffix="", dir=None):
+def make_temp_dir(prefix="plano-", suffix="", dir=None):
     if dir is None:
         dir = get_system_temp_dir()
 
-    return _tempfile.mkdtemp(prefix="plano-", suffix=suffix, dir=dir)
+    return _tempfile.mkdtemp(prefix=prefix, suffix=suffix, dir=dir)
 
 class temp_file:
     def __init__(self, suffix="", dir=None):
@@ -1451,25 +1483,65 @@ class temp_dir:
 
 ## Time operations
 
-def sleep(seconds, quiet=False):
-    _info(quiet, "Sleeping for {} {}", seconds, plural("second", seconds))
-
-    _time.sleep(seconds)
-
+# Unix time
 def get_time():
     return _time.time()
 
-def format_duration(duration, align=False):
-    assert duration >= 0
+# Python UTC time
+def get_datetime():
+    return _datetime.datetime.now(tz=_datetime.timezone.utc)
 
-    if duration >= 3600:
-        value = duration / 3600
+def parse_timestamp(timestamp, format="%Y-%m-%dT%H:%M:%SZ"):
+    if timestamp is None:
+        return None
+
+    datetime = _datetime.datetime.strptime(timestamp, format)
+    datetime = datetime.replace(tzinfo=_datetime.timezone.utc)
+
+    return datetime
+
+def format_timestamp(datetime=None, format="%Y-%m-%dT%H:%M:%SZ"):
+    if datetime is None:
+        datetime = get_datetime()
+
+    return datetime.strftime(format)
+
+def format_date(datetime=None):
+    if datetime is None:
+        datetime = get_datetime()
+
+    day = datetime.day
+    month = datetime.strftime("%B")
+    year = datetime.strftime("%Y")
+
+    return f"{day} {month} {year}"
+
+def format_time(datetime=None, precision="second"):
+    if datetime is None:
+        datetime = get_datetime()
+
+    assert precision in ("minute", "second"), "Illegal precision value"
+
+    hour = datetime.hour
+    minute = datetime.strftime("%M")
+    second = datetime.strftime("%S")
+
+    if precision == "second":
+        return f"{hour}:{minute}:{second}"
+    else:
+        return f"{hour}:{minute}"
+
+def format_duration(seconds, align=False):
+    assert seconds >= 0
+
+    if seconds >= 3600:
+        value = seconds / 3600
         unit = "h"
-    elif duration >= 5 * 60:
-        value = duration / 60
+    elif seconds >= 5 * 60:
+        value = seconds / 60
         unit = "m"
     else:
-        value = duration
+        value = seconds
         unit = "s"
 
     if align:
@@ -1478,6 +1550,11 @@ def format_duration(duration, align=False):
         return "{:.0f}{}".format(value, unit)
     else:
         return remove_suffix("{:.1f}".format(value), ".0") + unit
+
+def sleep(seconds, quiet=False):
+    _info(quiet, "Sleeping for {} {}", seconds, plural("second", seconds))
+
+    _time.sleep(seconds)
 
 class Timer:
     def __init__(self, timeout=None, timeout_message=None):
@@ -1593,7 +1670,7 @@ class Namespace:
 ## YAML operations
 
 def read_yaml(file):
-    check_module("yaml", "To install it, run 'pip install pyyaml'")
+    check_module("yaml", "Python module 'yaml' is not found.  To install it, run 'pip install pyyaml'.")
 
     import yaml as _yaml
 
@@ -1603,7 +1680,7 @@ def read_yaml(file):
         return _yaml.safe_load(f)
 
 def write_yaml(file, data):
-    check_module("yaml", "To install it, run 'pip install pyyaml'")
+    check_module("yaml", "Python module 'yaml' is not found.  To install it, run 'pip install pyyaml'.")
 
     import yaml as _yaml
 
@@ -1617,18 +1694,21 @@ def write_yaml(file, data):
     return file
 
 def parse_yaml(yaml):
-    check_module("yaml", "To install it, run 'pip install pyyaml'")
+    check_module("yaml", "Python module 'yaml' is not found.  To install it, run 'pip install pyyaml'.")
 
     import yaml as _yaml
 
     return _yaml.safe_load(yaml)
 
 def emit_yaml(data):
-    check_module("yaml", "To install it, run 'pip install pyyaml'")
+    check_module("yaml", "Python module 'yaml' is not found.  To install it, run 'pip install pyyaml'.")
 
     import yaml as _yaml
 
     return _yaml.safe_dump(data)
+
+def print_yaml(data, **kwargs):
+    print(emit_yaml(data), **kwargs)
 
 if PLANO_DEBUG: # pragma: nocover
     enable_logging(level="debug")
